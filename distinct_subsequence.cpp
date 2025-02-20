b5955293bc3a7063d0726bd05fb334ce1c0613bb
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2025-02-20 09:30:46 +0600

    Update distinct_subsequence.cpp

--- distinct_subsequence.cpp
+++ distinct_subsequence.cpp
@@ -18,7 +18,7 @@ int main()
       for(int i=0;i<n;i++)
       {
           char c=s[i];
-          sum=(total+1)%mod;  ///cur postion er jonno  value
+          sum=(total+1)%mod;  ///pore biyoger jonno store kortechi. dp[cur] er jonno,niche dekho
           if(mp.find(c)==mp.end())  ///samne ei char na pele viyog deoya
           ///lagbe na
           {
@@ -26,10 +26,24 @@ int main()
           }
           else
           {
-              total=(total%mod+(sum-mp[c]+mod)%mod)%mod;
+              total=(total%mod+(sum-mp[c]+mod)%mod)%mod; ///bigor kortechi kintu goto c er ta
           }
-          mp[c]=sum;
+          mp[c]=sum;   ///pore biyoger jonno store kortechi.
+
+
+
+
       }
       cout<<total+1<<endl;
     }
 }
+///abca
+/*
+dekho amar c er jonno joto gula distinct value hoicilo seta kintu last er a jonnoi hbe tai
+dp[4]=dp[3]+dp[2]+dp[1]+1; abca ekta subsequence tai jog korchi.
+eta just dp[4] er jonno
+amake to bolche total tai
+total=dp[4]+dp[3]+dp[2]+dp[1];
+bcdabcda
+4th a gula biyog dibo 8th er kache asle
+*/
